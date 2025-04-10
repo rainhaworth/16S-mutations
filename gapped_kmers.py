3fb9e76cf79e8500c9e2088d430312eb7c6c9013
rainhaworth <rhaworth@umd.edu>
2025-04-09 20:02:03 -0400

    added arg parsing, more analysis

--- gapped_kmers.py
+++ gapped_kmers.py
@@ -1,8 +1,10 @@
 from Bio import SeqIO
-from collections import defaultdict
 import numpy as np
-from utils.find_gapped import find_gapped
 import time
+import argparse
+import os
+
+from utils.find_gapped import find_gapped
 
 # create gapped kmers from universal kmers
 def get_univ_kmers(fn='./universal-residues-ecoli.txt', k=6, maxsize=100):
@@ -24,24 +26,29 @@ def get_univ_kmers(fn='./universal-residues-ecoli.txt', k=6, maxsize=100):
     return kmers
 
 # search sequences for candidate universal kmers
-def search_for_kmers(kmers, threshold=0.5, fn='./SILVA_138.2_SSURef_NR99_tax_silva_filtered.fasta'):
+def search_for_kmers(kmers, nmax=1000, threshold=0.5, fn='./SILVA_138.2_SSURef_NR99_tax_silva_filtered.fasta'):
     kmer_counts = np.zeros(len(kmers), dtype=int)
     kmers_of_interest = set()
     total_seqs = 0
+    seqs_none_found = 0
 
     # make numpy arrays
     kmer_chars = np.array([[x[0] for x in kmer] for kmer in kmers])
     kmer_offs = np.array([[x[1] for x in kmer] for kmer in kmers])
 
-    for h,i in enumerate(SeqIO.parse(fn, 'fasta')):
-        d,s,L = str(i.description), str(i.seq).upper().replace('T','U'), len(i.seq)
+    for i in SeqIO.parse(fn, 'fasta'):
+        d, s = str(i.description), str(i.seq).upper().replace('T','U')
         if 'eukaryota' in d.lower(): continue
 
         total_seqs += 1
-        kmer_counts += find_gapped(s, kmer_chars, kmer_offs)
+        found = find_gapped(s, kmer_chars, kmer_offs)
+        kmer_counts += found
+        if np.sum(found) == 0:
+            print('none found:', d)
+            seqs_none_found += 1
 
         print(total_seqs,end='\r')
-        if total_seqs == 100: break
+        if total_seqs == nmax: break
 
     for i,v in enumerate(kmer_counts):
         if v/total_seqs > threshold:
@@ -53,12 +60,27 @@ def search_for_kmers(kmers, threshold=0.5, fn='./SILVA_138.2_SSURef_NR99_tax_sil
     print(f"Sequences analyzed: {total_seqs}")
     print(f"Total k-mers found: {sum(kmer_counts)}")
     print(f"Number of biomarker k-mers identified: {len(kmers_of_interest)}")
+    print(f"Sequences containing no kmers: {seqs_none_found}")
     
     return kmers_of_interest, kmer_counts, kmer_counts / total_seqs
 
 if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('-k', type=int, default=8)
+    p.add_argument('-n', type=int, default=1000)
+    p.add_argument('-t', type=float, default=0.9)
+    p.add_argument('-i', type=str, default='./SILVA_138.2_SSURef_NR99_tax_silva_filtered.fasta')
+    p.add_argument('-o', type=str, default='./')
+    args = p.parse_args()
+
     st = time.time()
-    km = get_univ_kmers(k=8)
-    koi, kc, kf = search_for_kmers(km)
+    km = get_univ_kmers(k=args.k)
+    koi, kc, kf = search_for_kmers(km, args.n, args.t, args.i)
+    best = np.argmax(kf)
+
+    print(km)
     print(kf)
+    np.save(os.path.join(args.o, 'kf-'+str(args.k)+'-'+str(args.n)+'.o'), kf)
+
+    print('best:', kf[best], km[best])
     print('time:', time.time() - st)
