5da1fba7bff4000a2777c1fa1e68dacc62e9cf20
rainhaworth <rhaworth@umd.edu>
2025-04-24 21:34:46 -0400

    added quality and length filters

--- filter_silva_seqs.py
+++ filter_silva_seqs.py
@@ -1,15 +1,19 @@
 from Bio import SeqIO
 
 f = 'SILVA_138.2_SSURef_NR99_tax_silva.fasta'
-f_metadata = set(i.strip().split('\t')[0] for i in open('SILVA_138.2_SSURef_NR99.rnac.txt') if i.strip().split('\t')[5] == 'rRNA_16S')
+f_metadata = set(i.strip().split('\t')[1] for i in open('SILVA_138.2_SSURef_NR99.rnac')
+                 if i.strip().split('\t')[5] == 'rRNA_16S'
+                 and float(i.strip().split('\t')[8]) > 80
+                 and float(i.strip().split('\t')[9]) > 80
+                 and len(i.strip().split('\t')[-1]) > 1000)
 
 seqs_before_filtering = 0
 seqs_after_filtering = 0
 
-with open('SILVA_138.2_SSURef_NR99_tax_silva_filtered.fasta','w') as out:
+with open('SILVA_138.2_SSURef_NR99_tax_silva_filtered_strict.fasta','w') as out:
   for i in SeqIO.parse(f,'fasta'):
     seqs_before_filtering += 1
-    id,d,s = str(i.id).split('.')[0],str(i.description),str(i.seq).upper().replace('T','U')
+    id,d,s = '.'.join(str(i.id).split('.')[:2]),str(i.description),str(i.seq).upper().replace('T','U')
     if id not in f_metadata: # filter out sequences not labeled as 16S rRNA in SILVA metadata
       continue
     elif 'eukaryota' in d.lower(): # filter out eukaryotic sequences e.g., 18S rRNA or mitochondria/plastid 16S rRNA
